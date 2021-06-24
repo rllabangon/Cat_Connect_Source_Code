256827b3bf6fe2baedcf8d01a35b24e6621a2a9e
pcferrer1 <pcferrer1@up.edu.ph>
2021-06-24 08:27:22 +0800

    add cat and show cat using sqlalchemy

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 import urllib.request
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catconnect.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warnings
+db = SQLAlchemy(app)
+
+class Cats(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #primary key column, automatically generated IDs
+    name = db.Column(db.String(80), index=True, unique=False) # book title
+    #Checkpoint #1: insert your code here
+    breed = db.Column(db.String(80), index=True, unique=False) #author surname
+    age = db.Column(db.Integer, index=True, unique=False) #the month of book suggestion
+    description = db.Column(db.String(125), index=True, unique=False) #the year of book suggestion
+    image = db.Column(db.String(80), index=True, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Get a nice printout for Book objects
+    def __repr__(self):
+        return "{} in: {},{}".format(self.name, self.breed, self.age, self.description)
 
 UPLOAD_FOLDER = 'static/uploads/'
 
@@ -20,12 +40,18 @@ def allowed_file(filename):
 
 @app.route("/", methods=['GET','POST'])
 def hello_world():
-    return render_template('index.html')
+    our_cats = Cats.query.order_by(Cats.date_added)
+    return render_template('index.html', cats=our_cats)
 
 @app.route("/addcat", methods=['GET', 'POST'])
 def addcat():
 
     if request.method == 'POST':
+        cat_name = request.form['cat_name']
+        cat_breed = request.form['breed']
+        cat_age = request.form['age']
+        cat_desc = request.form['description']
+
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -38,10 +64,17 @@ def addcat():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             # print('upload_image filename: ' + filename)
             flash('Image successfully uploaded and displayed below')
-            return render_template('index.html', filename=filename)
+            # return render_template('index.html', filename=filename)
         else:
             flash('Allowed image types are - png, jpg, jpeg, gif')
             return redirect(request.url)
+
+        cat = Cats(name=cat_name, breed=cat_breed, age=cat_age, description=cat_desc, image=filename)
+        our_cats = Cats.query.order_by(Cats.date_added)
+        db.session.add(cat)
+        db.session.commit()
+        return render_template('index.html', cats=our_cats)
+
     else:
         return render_template('add_cat.html')
 
